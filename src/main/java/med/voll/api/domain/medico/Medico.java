b9c976d3835bb267c306d1b752c069ea6fae2328
Gabriel Evangelista <gabrielevangelista094@gmail.com>
2023-11-21 09:19:57 -0300

    Corrigindo formatação e adicionando toString

--- src/main/java/med/voll/api/domain/medico/Medico.java
+++ src/main/java/med/voll/api/domain/medico/Medico.java
@@ -1,61 +1,74 @@
 package med.voll.api.domain.medico;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import med.voll.api.domain.endereco.Endereco;
 
 @Table(name = "medicos")
 @Entity(name = "Medico")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Medico {
+public class Medico
+{
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
-    private String email;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String email;
 
-    private String telefone;
+	private String telefone;
 
-    private String crm;
+	private String crm;
 
-    @Enumerated(EnumType.STRING)
-    private Especialidade especialidade;
+	@Enumerated(EnumType.STRING)
+	private Especialidade especialidade;
 
-    @Embedded
-    private Endereco endereco;
+	@Embedded
+	private Endereco endereco;
 
-    private Boolean ativo;
+	private Boolean ativo;
 
-    public Medico(DadosCadastroMedico dados) {
-        this.ativo = true;
-        this.nome = dados.nome();
-        this.email = dados.email();
-        this.telefone = dados.telefone();
-        this.crm = dados.crm();
-        this.especialidade = dados.especialidade();
-        this.endereco = new Endereco(dados.endereco());
-    }
+	public Medico(DadosCadastroMedico dados)
+	{
+		this.ativo = true;
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.telefone = dados.telefone();
+		this.crm = dados.crm();
+		this.especialidade = dados.especialidade();
+		this.endereco = new Endereco(dados.endereco());
+	}
 
-    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
-        if (dados.nome() != null) {
-            this.nome = dados.nome();
-        }
-        if (dados.telefone() != null) {
-            this.telefone = dados.telefone();
-        }
-        if (dados.endereco() != null) {
-            this.endereco.atualizarInformacoes(dados.endereco());
-        }
+	@Override
+	public String toString()
+	{
+		return "Medico{" + "id=" + id + ", nome='" + nome + '\'' + ", email='" + email + '\'' + ", telefone='" + telefone + '\'' + ", crm='"
+				+ crm + '\'' + ", especialidade=" + especialidade + ", endereco=" + endereco + ", ativo=" + ativo + '}';
+	}
 
-    }
+	public void atualizarInformacoes(DadosAtualizacaoMedico dados)
+	{
+		if (dados.nome() != null)
+		{
+			this.nome = dados.nome();
+		}
+		if (dados.telefone() != null)
+		{
+			this.telefone = dados.telefone();
+		}
+		if (dados.endereco() != null)
+		{
+			this.endereco.atualizarInformacoes(dados.endereco());
+		}
 
-    public void excluir() {
-        this.ativo = false;
-    }
+	}
+
+	public void excluir()
+	{
+		this.ativo = false;
+	}
 }
